ccef76fb82fd3cda4e061e510ab909d5d3ed45d1
Watson-Dhrub <dhrubdubey5997@gmail.com>
2025-10-05 20:52:17 +0530

    prepare for github pages deployment

--- frontend/project/vite.config.ts
+++ frontend/project/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Nasa-Biology-Engine-/',
+  base: '/Mandel-s-Descendants/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
